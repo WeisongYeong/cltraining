ffd25d363d1839400f971b4bf6aedb1bd1bbe4e6
Wei Song <weisong0402@gmail.com>
2022-12-21 07:31:55 +0800

    second commit

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -18,16 +18,47 @@ const Theme = props => {
 }
 
 const Header = () =>{
+  const [url, setUrl] = React.useContext(MovieURLContext);
+  const [genres, setGenres] = useState();
+
+  useEffect(() => {
+    async function getAllGenres(url) {
+      const res = await axios.get(url);
+      setGenres(res.data.genres);
+    }
+    getAllGenres(url.genreURL)
+  }, []);
+
+  const concernedElement = document.querySelector(".click-text");
+
+  document.addEventListener("click", (e) => {
+    let search = document.getElementById('search');
+    let genreField = document.getElementById('genre-field');
+    if (search.contains(e.target) || genreField.contains(e.target)) {
+      genreField.style.transform = "translateY(0)";
+      genreField.style.transition = "transform 0.2s";
+    } else {
+      genreField.style.transform = "translateY(-100%)";
+      genreField.style.transition = "transform 0.2s";
+    }
+  });
+
   return (
     <header>
       <nav>
-        <div>Home</div>
+        <a className="home" href="#">Home</a>
         <form  id="form">
             <input type="text" placeholder="Search" id="search"></input>
         </form>
         <a className="login" href="#">Login</a>   {/*Comeback later for href*/}
       </nav>
-      <div id="genre-field"></div>
+      <div id="genre-field">
+        {genres && genres.map(genre => {
+          return(
+            <div key={genre.id} className="genre" id={genre.id}>{genre.name}</div>
+          )
+        })}
+      </div>
     </header>
   )
 }
@@ -52,38 +83,43 @@ const Main = () => {
 
   return (
     <main>
+      <h1>Top 20 Popular Movies</h1>
+      <div className="movie-container">
       {movies && movies.map(movie => {
         const {id, title, poster_path, vote_average, overview} = movie;
-        let imgURL = `${url.imgBaseURL}${poster_path}`;
+        let imgURL = `${url.imgBaseURL+poster_path}`;
+        let color = vote_average > 7 ? 'lightgreen' : (vote_average < 5? 'red' : 'orange');
         return (
-          <div key={id}>
-            <img src={imgURL}></img>
-            <div>{title}</div>
-            <div>{vote_average}</div>
-            <div>{overview}</div>
+          <div key={id} className="movie">
+            <img src={poster_path ? imgURL : "http://via.placeholder.com/1080x1580"} alt={title}></img>
+            <div className="movie-info">
+              <div className="title">{title}</div>
+              <div className="rating" style={{ color: color}}>{vote_average}</div>
+            </div>
+            {/* <div>
+              <div>Overview {overview ? '': 'Not Available'}</div>
+              <div>{overview}</div>
+            </div> */}
           </div>
         )
       })}
+      </div>
     </main>
   )
 }
 
 const Pagination = () => {
+
   return (
-    <div>
-      <button id="prev-page" type="button"> Next Page
-        <i className="material-icons">navigate_before</i>
-      </button>
+    <div className="pagination">
+      <button id="prev-page" type="button"> Prev </button>
       <div id="curr-page">1</div>
-      <button id="next-page" type="button"> Previous Page
-        <i className="material-icons">navigate_next</i>
-      </button>
+      <button id="next-page" type="button"> Next </button>
     </div>   
   )
 }
 
 
-// If error, check here as not using props as para
 const MovieURLProvider = props => {
   const apiKey = 'api_key=96f4f679b0cee46290970299c5656f9e';
   const baseURL = 'https://api.themoviedb.org/3';
